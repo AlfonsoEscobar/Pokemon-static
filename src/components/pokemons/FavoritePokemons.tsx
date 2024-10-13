a58c4f6d337324742c723fa5ee3ab4ccdec4a2f8
AlfonsoEscobar <Alfonso.Escobar.torrico@gmail.com>
2024-10-13 20:35:41 +0200

    Terminamos la aplicacion de los pokemon con islas y favoritos en el localStorage

--- src/components/pokemons/FavoritePokemons.tsx
+++ src/components/pokemons/FavoritePokemons.tsx
@@ -1,8 +1,24 @@
+import type { FavoritePokemon } from "@interfaces/favorite-pokemon";
+import { For, createSignal } from "solid-js";
+import { FavoritePokemonCard } from "./FavoritePokemonCard";
 
+const getLocalStoragePokemons = (): FavoritePokemon[] => {
+    const favoritePokemon = JSON.parse(localStorage.getItem('favorites') ?? '[]');
 
+    return favoritePokemon;
+}
 
 export const FavoritePokemons = () => {
 
-    
+    const [pokemons, setPokemons] = createSignal(getLocalStoragePokemons());
+
+
+    return (
+        <div class="grid grid-cols-2 sm:grid-cols-4">
+            <For each={ pokemons() }>
+                { (pokemon) =>  <FavoritePokemonCard pokemon={pokemon}/> }
+            </For>
+        </div>
+    )
 
 }
